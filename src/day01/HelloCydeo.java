76a88930f6b36804affc91d2bbdea8c370b68075
Yusuf <38029093+velespitt@users.noreply.github.com>
2024-12-13 14:09:48 +0300

    Update HelloCydeo.java

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -3,5 +3,7 @@ package day01;
 public class HelloCydeo {
     public static void main(String[] args) {
         System.out.println("Hello Cydeo");
+        System.out.println("Hello Cydeo");
+
     }
 }
